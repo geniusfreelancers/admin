0df3747b854670b539290faff98ce58e740affba
khanaldeepak1988 <khanaldeepak1988@gmail.com>
2018-06-15 17:03:27 -0400

    deleted pro images

--- src/main/java/com/adminportal/AdminportalApplication.java
+++ src/main/java/com/adminportal/AdminportalApplication.java
@@ -10,7 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import com.adminportal.domain.User;
 import com.adminportal.domain.security.Role;
 import com.adminportal.domain.security.UserRole;
@@ -18,6 +19,50 @@ import com.adminportal.service.StorageService;
 import com.adminportal.service.UserService;
 import com.adminportal.service.impl.StorageProperties;
 import com.adminportal.utility.SecurityUtility;
+
+
+
+
+
+/*@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+@ComponentScan({ "com.adminportal", "controller" })
+public class AdminportalApplication extends SpringBootServletInitializer {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private StorageService storageService;
+	 public static void main(String[] args) throws Exception {
+	        SpringApplication.run(AdminportalApplication.class, args);
+	    }
+	 
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		 User user1 = new User();
+			user1.setUsername("admins");
+			user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
+			user1.setEmail("admins@gmail.com");
+			Set<UserRole> userRoles = new HashSet<>();
+			Role role1 = new Role();
+			role1.setRoleId(0);
+			role1.setName("ROLE_ADMIN");
+			userRoles.add(new UserRole(user1, role1));
+			
+			userService.createUser(user1, userRoles);
+			storageService.init();
+	        return builder.sources(AdminportalApplication.class);
+	    }
+	 
+	 
+	 @Bean
+	    CommandLineRunner init(StorageService storageService) {
+	        return (args) -> {
+	          // storageService.deleteAll();
+	            storageService.init();
+	        };
+	    }
+
+}*/
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -31,22 +76,7 @@ public class AdminportalApplication implements CommandLineRunner {
 	        SpringApplication.run(AdminportalApplication.class, args);
 	    }
 	 
-	 /*@Override
-	 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		    User user1 = new User();
-			user1.setUsername("jainson");
-			user1.setUserType("Admin");
-			user1.setPassword(SecurityUtility.passwordEncoder().encode("98044Admin"));
-			user1.setEmail("kolystylefashion@gmail.com");
-			Set<UserRole> userRoles = new HashSet<>();
-			Role role1 = new Role();
-			role1.setRoleId(0);
-			role1.setName("ROLE_ADMIN");
-			userRoles.add(new UserRole(user1, role1));
-			
-			userService.createUser(user1, userRoles);
-	        return builder.sources(AdminportalApplication.class);
-	    }*/
+	
      @Override	 
 	 public void run(String... args) throws Exception{
 			User user1 = new User();
@@ -69,41 +99,5 @@ public class AdminportalApplication implements CommandLineRunner {
 	            storageService.init();
 	        };
 	    }
-
-
-
-/*
-import com.braintreegateway.BraintreeGateway;
-
-@SpringBootApplication
-public class AdminportalApplication implements CommandLineRunner{
-
-//	public static String DEFAULT_CONFIG_FILENAME = "config.properties";
-//    public static BraintreeGateway gateway;
-    
-	@Autowired
-	private UserService userService;
-	
-	public static void main(String[] args) {
-
-		SpringApplication.run(AdminportalApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception{
-		User user1 = new User();
-		user1.setUsername("admin");
-		user1.setUserType("Admin");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
-		user1.setEmail("admin@gmail.com");
-		Set<UserRole> userRoles = new HashSet<>();
-		Role role1 = new Role();
-		role1.setRoleId(0);
-		role1.setName("ROLE_ADMIN");
-		userRoles.add(new UserRole(user1, role1));
-		
-		userService.createUser(user1, userRoles);
-	}
-	*/
 
 }
